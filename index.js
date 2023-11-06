1387818c6bd3b387475c42454befe2fa01811698
TamannaJahan55 <tamannajahan.mis049@gmail.com>
2023-11-06 22:41:26 +0600

    inserted bulk data in database and loaded assignments data

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+
+
+        const assignmentCollection = client.db('groupDB').collection('assignments');
+
+        app.get('/assignments', async(req, res) => {
+            const cursor = assignmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
