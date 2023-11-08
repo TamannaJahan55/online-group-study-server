7b6ed8d3bf849567fa1c6c8cc41c80bb2c522468
TamannaJahan55 <tamannajahan.mis049@gmail.com>
2023-11-09 01:55:20 +0600

    delete api created to delete a single submitted assignment

--- index.js
+++ index.js
@@ -112,6 +112,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/submittedAssignments/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id) }
+            const result = await submitCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
